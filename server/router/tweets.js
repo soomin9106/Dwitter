b33b791bdcf5cefe6dc41deedc3e3271ba3da345
soomin9106 <soomin9106@naver.com>
2021-07-17 11:32:29 +0900

    Dwitter : Validation

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js';
+import { body,param,validationResult } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 
 const router =  express.Router();
+
+//validate tweets.
+const validateTweet = [
+    body('text')
+    .trim()
+    .isLength({min:4})
+    .withMessage('text should be at least 4 characters.'),
+    validate,
+];
+
+
 
 //GET /tweets
 //GET /tweets?username=:username
@@ -11,9 +24,9 @@ router.get('/',tweetController.getTweets);
 //GET /tweets/:id
 router.get('/:id', tweetController.getTweetsById)
 //POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/',validateTweet,tweetController.createTweet);
 //PUT /tweets/id
-router.put('/:id',tweetController.updateTweet);
+router.put('/:id',validateTweet,tweetController.updateTweet);
 //DELETE /tweets/1
 router.delete('/:id',tweetController.removeTweet);
 export default router;
