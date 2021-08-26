9fd3707cf66650fae34f56dac36b47b9b579a755
soomin9106 <soomin9106@naver.com>
2021-08-26 21:22:02 +0900

    using sequelize

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import tweetsRouter from './router/tweets.js'
 import authRouter from './router/auth.js'
 import { config } from './config.js';
 import {initSocket} from './connection/socket.js';
+import { sequelize } from './db/database.js';
 
 
 const app=express();
@@ -29,5 +30,8 @@ app.use((error,req,res,next) => {
     res.sendStatus(500);
 });
 
-const server = app.listen(config.host.port);
-initSocket(server);
+sequelize.sync().then(client => {
+    //console.log(client);
+    const server = app.listen(config.host.port);
+    initSocket(server);
+});
